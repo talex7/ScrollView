21bd2ab3a56258c40c378cc1ad0e57ed04db2241
talex7 <talexanian@gmail.com>
2016-11-14 16:43:45 -0500

    Initial Commit

---
+++ ScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView
+//
+//  Created by Thomas Alexanian on 2016-11-14.
+//  Copyright © 2016 Thomas Alexanian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollView
+//
+//  Created by Thomas Alexanian on 2016-11-14.
+//  Copyright © 2016 Thomas Alexanian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
